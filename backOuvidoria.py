fed2bac8cdd687fef17111a936616e4990b7c03e
Otavio <otavio1390@gmail.com>
2025-04-13 13:54:45 -0300

    pesquisar por id e categoria

--- backOuvidoria.py
+++ backOuvidoria.py
@@ -70,5 +70,40 @@ def excluirManifestacao(conn):
     print("Manifestação excluída com sucesso!")
 # excluirManifestacao(conn)
 
+def pesquisarPorId(conn):
+    id_manifestacao = int(input("Digite o ID da manifestação que deseja pesquisar: ").strip())
+    consulta = "SELECT * FROM manifestacoes WHERE id = %s"
+    dados = (id_manifestacao,)
+    manifestacao = listarBancoDados(conn, consulta, dados)
+
+    if manifestacao:
+        for m in manifestacao:
+            print(f"\n ID: {m[0]} \n Tipo: {m[1]} \n Descrição: {m[2]}")
+    else:
+        print("Nenhuma manifestação encontrada com o ID fornecido.")
+
+def filtrarPorCategoria(conn):
+    print("Escolha o tipo de manifestação para filtrar:")
+    tipos = ["Reclamação", "Sugestão", "Elogio", "Denúncia", "Outros"]
+    for i, tipo in enumerate(tipos, start=1):
+        print(f"{i} - {tipo}")
+
+    while True:
+        tipo_opcao = input("Digite o número correspondente ao tipo: ").strip()
+        if tipo_opcao.isdigit() and 1 <= int(tipo_opcao) <= len(tipos):
+            tipo_escolhido = tipos[int(tipo_opcao) - 1]
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+    consulta = "SELECT * FROM manifestacoes WHERE categoria = %s"
+    dados = (tipo_escolhido,)
+    manifestacoes = listarBancoDados(conn, consulta, dados)
+
+    if manifestacoes:
+        for manifestacao in manifestacoes:
+            print(f"\n ID: {manifestacao[0]} \n Tipo: {manifestacao[1]} \n Descrição: {manifestacao[2]}")
+    else:
+        print(f"Nenhuma manifestação encontrada para o tipo '{tipo_escolhido}'.")
 
 encerrarConexao(conn)
--- ouvidoria.py
+++ ouvidoria.py
@@ -14,10 +14,12 @@ opcao = 0
 while True:
     print("=" * 35)
     print(  
-        "Opcão 1: Registrar Manifestação\n"
-        "Opcão 2: Listar Manifestações\n"
-        "Opcão 3: Excluir Manifestação\n"
-        "Opcão 4: Sair"
+        "Opcão 1: Registrar manifestação\n"
+        "Opcão 2: Listar todas as manifestações\n"
+        "Opcão 3: Excluir manifestação\n"
+        "Opção 4: Pesquisar por ID\n"
+        "Opção 5: Pesquisar por Categoria\n"
+        "Opção 6: Sair"
         )
     print("=" * 35)
     opcao = int(input("Digite a opção desejada: "))
@@ -32,6 +34,12 @@ while True:
         excluirManifestacao(conn)
 
     elif opcao == 4:
+        pesquisarPorId(conn)
+        
+    elif opcao == 5:
+        filtrarPorCategoria(conn)
+
+    elif opcao == 6:
         print("Sair")
         break
 
